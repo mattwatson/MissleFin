19a73fb7d69117d79befa96f974e639710bfefcd
mattwatson <mattwatson1@gmail.com>
2015-12-26 17:35:28 +0000

    Add basic domain types

---
+++ MissleFin.Domain/DomainModel.fs
@@ -0,0 +1,19 @@
+﻿namespace MissleFin
+
+[<AutoOpen>]
+module DomainModel = 
+
+  type AccountType = Asset | Liability | Income | Expense | Payable | Receivable | Equity
+
+  [<CLIMutable>]
+  type AccountTag = 
+      { Id : int 
+        Name : string }
+
+  [<CLIMutable>]
+  type Account =
+      { Id : int
+        Name : string
+        Enabled : bool
+        Type : AccountType
+        Tags : int list }
